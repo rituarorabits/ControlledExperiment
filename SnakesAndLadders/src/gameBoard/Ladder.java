1e16e084d252503af07565bddd45253e23a08162
rituarora2013 <mailtorituarora@yahoo.com>
2017-02-08 12:56:18 +0530

    Adding initializeBoard Class

--- SnakesAndLadders/src/gameBoard/Ladder.java
+++ SnakesAndLadders/src/gameBoard/Ladder.java
@@ -2,4 +2,14 @@ package gameBoard;
 
 public class Ladder {
 
+	int startPosition;
+	int endPosition;
+	int colorElement;
+	
+	Ladder(int start, int end, int color) 
+	{
+		startPosition= start;
+		endPosition= end;
+		colorElement= color;
+	}
 }

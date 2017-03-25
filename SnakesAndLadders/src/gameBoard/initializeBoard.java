ea325e574098817e88332c327bf931a8f8a7f446
rituarora2013 <mailtorituarora@yahoo.com>
2017-03-25 12:48:16 +0530

    Second commit by megha

--- SnakesAndLadders/src/gameBoard/initializeBoard.java
+++ SnakesAndLadders/src/gameBoard/initializeBoard.java
@@ -1,6 +1,28 @@
 package gameBoard;
 
 public class initializeBoard {
-
+  boolean checkValidity(Snake s){
+	if(  s.startPosition>s.endPosition)
+	{
+	   return true;
+	}
+	
+	else
+	{
+		return false;
+	}
+  
+	  
+  }
+  
+  void initSnakes(Snake s)
+  {
+	  try{
+		  Snake a= new Snake(12,13,2);
+		  
+	  }
+	  
+	  catch(IllegalArgumentException e){}
+  }
 	
 }
